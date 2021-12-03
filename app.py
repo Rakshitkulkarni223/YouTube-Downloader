63796adf015c58b5bfdb5657478e0c518440be6a
Rakshitkulkarni223 <76918993+Rakshitkulkarni223@users.noreply.github.com>
2021-12-03 14:33:22 +0530

    Updated

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, request, render_template, send_file
 from pytube import YouTube
 import logging,sys
+import instagram
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 app = Flask(__name__)
 
+resolutions=['Auto','144p','240p','360p','480p','720p','1080p','1440p','2040p']
 
 @app.route("/")
 def home():
-    return render_template('MP4.html')
+    return render_template('Home.html')
 
+# YouTube
 
 @app.route("/Download_MP4")
 def Download_MP4():
-    return render_template('MP4.html')
+    global resolutions
+    return render_template('MP4.html', resolutions=resolutions)
 
 
 @app.route("/Download_MP3")
@@ -24,14 +28,44 @@ def Download_MP3():
 @app.route('/download_video', methods=['GET', 'POST'])
 def download_video():
     try:
+        global filesize
         input_url = request.form['URL']
-        
-        video = YouTube(str(input_url)).streams.filter(resolution="720p", file_extension='mp4').first()
-        
+
+        resolu = set()
+
+        for stream in YouTube(input_url).streams.filter(progressive=True).order_by('resolution'):
+            resolu.add(stream.resolution)
+
+        resolu=sorted(list(resolu))
+
+        res=request.form['resolution']
+
+        flag=False
+
+        for resolution in resolu:
+            if res==resolution:
+                flag=True
+                video = YouTube(str(input_url)).streams.filter(progressive=True, resolution=resolution, file_extension='mp4').first()
+                break
+
+        if not flag:
+            video = YouTube(str(input_url)).streams.filter(progressive=True, resolution=resolu[len(resolu)-1],
+                                                           file_extension='mp4').first()
+
+
         name = video.title
-        
-        video.download(output_path='download/', filename=name + '.mp4')
-        
+
+        # filesize=video.filesize
+
+        # print(filesize)
+        #
+        # check()
+
+        if '|' in name:
+            name=name.split('|')[0]
+
+        video.download(output_path='download', filename=name + '.mp4')
+
         return send_file(f"download/{name}.mp4",as_attachment=True)
     except:
         logging.exception('Failed download')
@@ -48,7 +82,10 @@ def download_audio():
 
         name = audio_file.streams.get_audio_only().title
 
-        audio_file.streams.get_audio_only().download(output_path='download/', filename=name + '.mp3')
+        if '|' in name:
+            name=name.split('|')[0]
+
+        audio_file.streams.get_audio_only().download(output_path='download', filename=name + '.mp3')
 
         return send_file(f"download/{name}.mp3",as_attachment=True)
     
@@ -57,5 +94,35 @@ def download_audio():
         return 'Audio download failed!'
 
 
+# Instagram
+
+@app.route("/Download_reel")
+def Download_reel():
+    return render_template('Instagram_reel.html')
+
+
+@app.route("/Download_post")
+def Download_post():
+    return render_template('Instagram_post.html')
+
+@app.route("/download_insta_reel", methods=['GET', 'POST'])
+def download_insta_reel():
+    try:
+        input_url = request.form['URL']
+        username = "rakshit__kulkarni"
+        password = "196219992002@rdks"
+        filename=instagram.Download_reel(input_url,username,password)
+
+        if filename=="None":
+            logging.exception('Failed download')
+            return 'Instagram Reel download failed!'
+
+        return send_file(f"download\\{filename}.mp4", as_attachment=True)
+    except:
+        logging.exception('Failed download')
+        return 'Instagram Reel download failed!'
+
+
 if __name__ == '__main__':
+    # filesize=0
     app.run(debug=True)
---
+++ instagram.py
@@ -0,0 +1,79 @@
+import json
+from datetime import datetime
+import requests
+from instascrape import Reel
+
+login_response={}
+json_data={}
+
+def authontication(username,password):
+    global login_response, json_data
+
+    link = 'https://www.instagram.com/accounts/login/'
+    login_url = 'https://www.instagram.com/accounts/login/ajax/'
+
+    time = int(datetime.now().timestamp())
+    response = requests.get(link)
+    csrf = response.cookies['csrftoken']
+
+    payload = {
+        'username': username,
+        'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time}:{password}',
+        'queryParams': {},
+        'optIntoOneTap': 'false'
+    }
+
+    login_header = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
+        "X-Requested-With": "XMLHttpRequest",
+        "Referer": "https://www.instagram.com/accounts/login/",
+        "x-csrftoken": csrf
+    }
+
+    login_response = requests.post(login_url, data=payload, headers=login_header)
+    json_data = json.loads(login_response.text)
+
+    return login_response,json_data
+
+def Download_reel(url,username,password):
+    global login_response, json_data
+
+    if len(json_data)==0:
+        print("Ok")
+        res=authontication(username,password)
+        login_response=res[0]
+        json_data=res[1]
+
+    try:
+        if json_data.get("authenticated") != None:
+            cookies = login_response.cookies
+            cookie_jar = cookies.get_dict()
+            csrf_token = cookie_jar['csrftoken']
+            print("csrf_token: ", csrf_token)
+            session_id = cookie_jar['sessionid']
+            print("session_id: ", session_id)
+            print("login successful")
+
+            # Header with session id
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+            	AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.74 \
+            	Safari/537.36 Edg/79.0.309.43",
+                "cookie": f'sessionid={session_id};'
+            }
+
+            # Passing Instagram reel link as argument in Reel Module
+            insta_reel = Reel(url)
+
+            # Using scrape function and passing the headers
+            insta_reel.scrape(headers=headers)
+
+            video_name = (url.split('/'))[4]
+
+            # Giving path where we want to download reel to the
+            # download function
+            insta_reel.download(fp=f"download\\{video_name}.mp4")
+
+            return video_name
+    except:
+        return "None"
